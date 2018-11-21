1214f9150f4899964b13c8d0750fe47075d9c937
Jan Biasi <jan.biasi@namics.com>
2018-11-21 14:44:43 +0100

    feat(angular-plugin): add fixture for integration test (e2e)

--- packages/plugins/angular/src/Platform.ts
+++ packages/plugins/angular/src/Platform.ts
@@ -1,26 +1,28 @@
-import { PlatformRef, StaticProvider, NgModuleRef } from '@angular/core/';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { NgModuleFactory } from '@angular/core/src/render3';
+import { PlatformRef, StaticProvider, NgModuleRef } from "@angular/core/";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { NgModuleFactory } from "@angular/core/src/render3";
 
 enum PlatformDefs {
-    Dynamic
+  Dynamic
 }
 
 interface NgWindow {
-    ngRef: NgModuleRef<any>
+  ngRef: NgModuleRef<any>;
 }
 
 export const hot = (boot: Promise<NgModuleFactory<any>>) => {
-    boot.then((ref: NgModuleFactory<any>) => {
-        // Ensure Angular destroys itself on hot reloads.
-        const ngRef: NgModuleRef<any> | undefined = (window as any).ngRef;
-        if (ngRef) {
-        (ngRef as NgModuleRef<any>).destroy();
-        }
-        (window as any)['ngRef'] = ref;
-    });
-}
+  boot.then((ref: NgModuleFactory<any>) => {
+    // Ensure Angular destroys itself on hot reloads.
+    const ngRef: NgModuleRef<any> | undefined = (window as any).ngRef;
+    if (ngRef) {
+      (ngRef as NgModuleRef<any>).destroy();
+    }
+    (window as any)["ngRef"] = ref;
+  });
+};
 
-export const Platform: { [key in PlatformDefs]: (providers: StaticProvider[] | undefined) => PlatformRef } = {
-    [PlatformDefs.Dynamic]: platformBrowserDynamic
-}
+export const Platform: {
+  [key in PlatformDefs]: (providers: StaticProvider[] | undefined) => PlatformRef
+} = {
+  [PlatformDefs.Dynamic]: platformBrowserDynamic
+};
